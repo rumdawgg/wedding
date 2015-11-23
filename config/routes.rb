a4eef740aafb45e04fd37020e4afc13e0088a0bc
Paul Chicarello <pchicarello@patientslikeme.com>
2015-11-23 12:45:17 -0500

    fixed up master branch. got at least all navigation working - even if it is just to dummy pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root    'static#home'
-   get    'story'    => 'static#story'
-   get    'event'    => 'static#event'
-   get    'registries'    => 'static#registries'
-   get    'photos'     => 'static#photos'
+   get    'story'       => 'static#story'
+   get    'photos'      => 'static#photos'
+   get    'cats'        => 'static#cats'
+   get    'event'       => 'static#event'
+   get    'registries'  => 'static#registries'
+   get    'rsvp'        => 'static#rsvp'
 end
