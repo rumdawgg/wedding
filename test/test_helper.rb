be8d97d9141217fedf5534912826330d69a6c7c9
Paul Chicarello <pchicarello@patientslikeme.com>
2015-12-02 21:08:30 -0500

    got Travis working, now trying to get it to report to code climate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
-require "codeclimate-test-reporter"
+
 Minitest::Reporters.use!
-CodeClimate::TestReporter.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
