5d3a6be756c6748babe42c61d25263419b0fc5e3
Paul Chicarello <pchicarello@patientslikeme.com>
2015-12-05 18:35:46 -0500

    removed title attribute

--- db/migrate/20151204195948_create_photos.rb
+++ db/migrate/20151204195948_create_photos.rb
@@ -1,7 +1,6 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-      t.string :title
       t.string :caption
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20151204203900) do
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
-    t.string   "title"
     t.string   "caption"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
