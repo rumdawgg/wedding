e07dafb684491735d175b94b62c71a826f16b384
Paul Chicarello <pchicarello@patientslikeme.com>
2016-04-13 13:20:43 -0400

    Only enable rollbar on production servers

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,11 +1,12 @@
+require 'rollbar/rails'
 Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
   config.access_token = '78a045fd595846199397295062ffa7a1'
 
-  if Rails.env.test? ||  Rails.env == 'stage' || Rails.env == 'development'
+  unless Rails.env.production?
     config.enabled = false
   end
-  
+
 end
