1f257da95d0811607b78fae49ab2961975fc914a
Paul Chicarello <pchicarello@patientslikeme.com>
2016-05-02 20:08:48 -0400

    Invitees controller is now pretty much functional

--- app/controllers/invitees_controller.rb
+++ app/controllers/invitees_controller.rb
@@ -4,11 +4,6 @@ class InviteesController < ApplicationController
     @invitees = Invitee.all
   end
 
-  def get_meal(meal)
-    @meal = Meal.find(meal)
-    return @meal.name
-  end
-
   def show
     @invitee = Invitee.find(params[:id])
     @guests = @invitee.guests
@@ -17,28 +12,20 @@ class InviteesController < ApplicationController
 
   def new
     @invitee = Invitee.new
-    @invitee.guests.new # build ingredient attributes, nothing new here
   end
 
   def create
     @invitee = Invitee.new(invitee_params)
-    if params[:add_guest]
-      @guest = @invitee.guests.build
-    elsif params[:remove_ingredient]
-    else
-      if @invitee.save
-        flash[:notice] = "Registration Sucessful."
-        redirect_to @invitee and return
-      end
+    if @invitee.save
+      flash[:notice] = "Registration Sucessful."
+      redirect_to @invitee and return
     end
-    render :action => 'new'
+      render 'new'
   end
-
-  helper_method :get_meal
 
   private
   def invitee_params
-        params.require(:invitee).permit(:first_name,:last_name,guests_attributes:[:first_name,:last_name,:meal_id,:_destroy,:id])
+    params.require(:invitee).permit(:first_name,:last_name,:meal_id,guests_attributes:[:first_name,:last_name,:meal_id,:_destroy,:id])
   end
 
 end
