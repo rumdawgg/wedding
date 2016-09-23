f8b076a8a26329889b2309dd04130276c064d22f
Paul Chicarello <pchicarello@patientslikeme.com>
2016-09-22 21:52:18 -0400

    More fixes to try and get stage deploys working again

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,4 +1,4 @@
 set :rails_env, 'stage'
 set :stage, 'stage'
 
-server 'aria.lan.chicarello.com', roles: %w{web app db}, ssh_options: { user_known_hosts_file: '/dev/null' }
+server 'winterfell.lan.chicarello.com', roles: %w{web app db}, ssh_options: { user_known_hosts_file: '/dev/null' }
--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -1,16 +1,22 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.serve_static_files = false
-  config.assets.js_compressor  = Uglifier.new(compress: {unused: false})
+  config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.digest = true
-  config.log_level = :debug
-  config.force_ssl = false
+  config.assets.version = '1.0'
+  config.log_level = :info
+  config.colorize_logging = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_record.dump_schema_after_migration = false
+  config.active_record.raise_in_transactional_callbacks = false
+  host = ENV["SERVER_HOSTNAME"] || 'localhost'
+  ActionMailer::Base.delivery_method = :sendmail
+  config.action_mailer.asset_host = "http://#{host}"
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
